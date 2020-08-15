c1a04d02ff1845e1664a35978547f7e6e5bed099
THARUVI <tharuvi98@gmail.com>
2020-08-15 23:17:55 +0530

    integrated commit

--- app/src/main/java/com/example/mywedding/editGuest.java
+++ app/src/main/java/com/example/mywedding/editGuest.java
@@ -1,8 +1,14 @@
 package com.example.mywedding;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -27,5 +33,33 @@ public class editGuest extends AppCompatActivity {
                 }
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_save,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.done){
+            Intent intent = new Intent(editGuest.this,allGuests.class);
+            startActivity(intent);
+
+            Context context = getApplicationContext();
+            CharSequence message = "Changes Saved";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, message, duration);
+            toast.show();
+        }
+
+        if(id == android.R.id.home){
+            Intent intent = new Intent(editGuest.this,viewGuest.class);
+            startActivity(intent);
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/mywedding/guestPage1.java
+++ app/src/main/java/com/example/mywedding/guestPage1.java
@@ -12,6 +12,7 @@ public class guestPage1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_page1);
+        getSupportActionBar().setTitle(R.string.guestpage1);
     }
 
 
