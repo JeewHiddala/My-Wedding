1eb6c1807652247f0c91af6fc5cf63a974830259
THARUVI <tharuvi98@gmail.com>
2020-08-15 13:08:45 +0530

    Guest management second commit

--- app/src/main/java/com/example/mywedding/editGuest.java
+++ app/src/main/java/com/example/mywedding/editGuest.java
@@ -3,12 +3,29 @@ package com.example.mywedding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class editGuest extends AppCompatActivity {
+
+    Switch aSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_guest);
+
+        aSwitch = (Switch) findViewById(R.id.switch1);
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b == true){
+                    Toast.makeText(getBaseContext(),"Sent",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getBaseContext(),"Not Sent",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
