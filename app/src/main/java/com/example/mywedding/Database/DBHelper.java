e7124322ac6dcc2df8188928b5bb9ea7b98ddeb0
NishanNiriella <nishandilip12@gmail.com>
2020-09-18 14:17:51 +0530

    DB Package Created

---
+++ app/src/main/java/com/example/mywedding/Database/DBHelper.java
@@ -0,0 +1,23 @@
+package com.example.mywedding.Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "MyWed.db";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
